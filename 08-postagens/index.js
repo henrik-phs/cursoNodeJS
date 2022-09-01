72599635d6aceb8c8d209be0b145ea3088a51505
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2022-09-01 18:51:45 -0300

    salvando dados do formulário no banco de dados

--- 08-postagens/index.js
+++ 08-postagens/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
+const Post = require('./models/Post')
 
 // CONFIGURA O HANDLEBARS
 app.engine('handlebars', engine())
@@ -13,13 +14,23 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // ROTAS
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.get('/enviar-post', (req, res) => {
     res.render('postagem')
 })
 
 app.post('/postar', (req, res) => {
-    res.send("Titulo: " + req.body.titulo + "\nConteudo: " + req.body.conteudo)
+    Post.create({
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo
+    }).then(() => {
+        res.redirect('/')
+    }).catch((erro) => {
+        res.send('Erro ao enviar dados: ' + erro)
+    })
 })
 
 app.listen(8080, () => {
