d28f14a441bc2bb66faabb6a13effa0057c2a392
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2022-09-05 15:42:26 -0300

    criando usuario com mongoose

--- 09-mongoose/aprendendo.js
+++ 09-mongoose/aprendendo.js
@@ -2,10 +2,48 @@ const mongoose = require('mongoose')
 
 // CONFIGURANDO O MONGOOSE
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/teste', {
-    useMongoClient: true
-}).then(() => {
+mongoose.connect('mongodb://localhost/teste').then(() => {
     console.log("DB Conectado...")
 }).catch((err) => {
     console.log("Erro ao conectar: " + err)
+})
+
+// MODELS
+// DEFININDO O MODEL
+const UserSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number
+    },
+    pais: {
+        type: String
+    }
+})
+
+// DEFININDO A COLECTION
+mongoose.model('usuarios', UserSchema)
+
+// INSERINDO DADOS
+const Usuario = mongoose.model('usuarios')
+new Usuario({
+    nome: 'John',
+    sobrenome: 'Doe',
+    email: 'john@doe.com',
+    idade: 20,
+    pais: 'EUA'
+}).save().then(() => {
+    console.log('Usuário criado com sucesso')
+}).catch((err) => {
+    console.log('Eroo ao inserir: ' + err)
 })
