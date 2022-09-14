c5947af6bf3f35fd6eb80ae90ff9334f8ca663bd
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2022-09-13 21:19:30 -0300

    cadastro de categorias

--- 10-projeto-express/app.js
+++ 10-projeto-express/app.js
@@ -1,4 +1,6 @@
-// IMPORTANDO MÓDULOS
+/**
+ *  IMPORTANDO MÓDULOS
+ */
 const express = require('express')
 const { engine } = require('express-handlebars')
 const bodyParser = require('body-parser')
@@ -6,15 +8,20 @@ const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
 
-const path = require('path')
-    // const mongose = require('mongoose')
+const path = require('path') // MÓDULO RESPONSAVEL POR TRABALHAR COM DIRETÓRIOS
+const mongose = require('mongoose')
 
-// CONFIGURAÇÕES
-// BODY PARSER
+/**
+ * CONFIGURAÇÕES
+ * BODY PARSER - TRABALHA COM DADOS VINDOS DE FORMULÁRIO E TRANSFORMA EM JSON
+ */
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-// HANDLEBARS
+/**
+ * HANDLEBARS
+ * TEMPLATE ENGINE
+ */
 app.engine('handlebars', engine({
     defaultLayout: 'main',
     runtimeOptions: {
@@ -25,13 +32,31 @@ app.engine('handlebars', engine({
 app.set('view engine', 'handlebars')
 app.set('views', '10-projeto-express/views')
 
-// PUBLIC
+/**
+ * CONFIGURAÇÕES DO MONGOOSE
+ * RESPONSÁVEL PELA CONEÇÃO COM BANCO DE DADOS MONGODB
+ */
+mongose.Promise = global.Promise
+mongose.connect("mongodb://localhost/blogapp").then(() => {
+    console.log("conectado ao mongo")
+}).catch((err) => {
+    console.log("erro ao conectar: " + err)
+})
+
+/**
+ * PUBLIC
+ * INFORMA AO EXPRESS QUE A PASTA PUBLIC É A PASTA DE ARQUIVOS ESTÁTICOS
+ */
 app.use(express.static(path.join(__dirname, "public")))
 
-// ROTAS
+/**
+ * ROTAS
+ */
 app.use('/admin', admin)
 
-// OUTROS
+/**
+ * RESPONSÁVEL POR CRIAR UM SERVIDO NODE E RODAR O PROGRAMA NO NAVEGADOR
+ */
 app.listen(3000, () => {
     console.log('servidor rodando')
 })
