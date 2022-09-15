c5f03ad99c3f4366213ec790993b705a7af2f95c
henrik-phs <pedrohenriquedasilva100@yahoo.com.br>
2022-09-15 19:24:27 -0300

    deletar postagem

--- 10-projeto-express/routes/admin.js
+++ 10-projeto-express/routes/admin.js
@@ -17,7 +17,13 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-    res.render('admin/categorias')
+    Categoria.find().sort({ date: 'desc' }).then((categorias) => {
+        res.render('admin/categorias', { categorias: categorias })
+    }).catch((err) => {
+        req.flash('error_msg', 'Houve um erro ao listar as categorias ' + err)
+        res.redirect('/admin')
+    })
+
 })
 
 router.get('/categorias/add', (req, res) => {
@@ -58,4 +64,58 @@ router.post('/categorias/nova', (req, res) => {
 
 })
 
+router.get('/categorias/edit/:id', (req, res) => {
+    Categoria.findOne({ _id: req.params.id }).then((categoria) => {
+        res.render('admin/edit-categorias', { categoria: categoria })
+    }).catch((err) => {
+        req.flash('error_msg', 'Essa categoria não existe ' + err)
+        res.redirect('/admin/categorias')
+    })
+})
+
+router.post('/categorias/edit', (req, res) => {
+    var erros = []
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({ texto: 'Nome inválido' })
+    }
+
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({ texto: 'Slug inválido' })
+    }
+
+    if (req.body.nome.length < 2) {
+        erros.push({ texto: 'Nome da categoria muito pequeno' })
+    }
+
+    if (erros.length > 0) {
+        res.render('admin/add-categorias', { erros: erros })
+    } else {
+        Categoria.findOne({ _id: req.body.id }).then((categoria) => {
+            categoria.nome = req.body.nome
+            categoria.slug = req.body.slug
+
+            categoria.save().then(() => {
+                req.flash('success_msg', 'Categoria editada com sucesso')
+                res.redirect('/admin/categorias')
+            }).catch((err) => {
+                req.flash('error_msg', 'Erro ao salvar categoria ' + err)
+                res.redirect('/admin/categorias')
+            })
+        }).catch((err) => {
+            req.flash('error_msg', 'Houve um erro ao editar a categoria')
+            res.redirect('/admin/categorias')
+        })
+    }
+})
+
+router.post('/categorias/deletar/:id', (req, res) => {
+    Categoria.remove({ _id: req.body.id }).then(() => {
+        req.flash('success_msg', 'Categoria deletada com sucesso')
+        res.redirect('/admin/categorias')
+    }).catch((err) => {
+        req.flash('error_msg', 'Erro ao deletar categoria ' + err)
+        res.redirect('/admin/categorias')
+    })
+})
 module.exports = router
